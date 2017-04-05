0a6162ee9bc2b69db0f327074f1714b38110d5f0
sonam0918 <sonam.snj0918@gmail.com>
2017-04-04 23:24:26 -0700

    API call
    API call

--- routes/index.js
+++ routes/index.js
@@ -1,24 +1,82 @@
 var express = require('express');
 var router = express.Router();
 var path = require('path');
+var http = require('http');
+var global_var = require('../public/javascripts/validate');
  
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.sendFile(path.join(__dirname, '../', 'views', 'index.html'));
 });
  
-/* router.get('/data', function(req,res){
-	res.json([{"id": 1, "name": "Mymm", "city": "Pantano do Sul"},
-        {"id": 2, "name": "Skyble", "city": "Guilmaro"},
-        {"id": 3, "name": "Tagfeed", "city": "Gnosjö"},
-        {"id": 4, "name": "Realcube", "city": "Jrashen"},
-        {"id": 5, "name": "Bluejam", "city": "Zhangjiawo"},
-        {"id": 6, "name": "Jayo", "city": "Obonoma"},
-        {"id": 7, "name": "Cogidoo", "city": "Sungsang"},
-        {"id": 8, "name": "Avavee", "city": "Diawara"},
-        {"id": 9, "name": "Tagtune", "city": "Monywa"},
-        {"id": 10, "name": "Centimia", "city": "Retkovci"}]);
-});
- 
- */
+
+ //Setting API route with paramters
+
+router.get('/products/:usertype/:productid', function(req,res){
+        var myProducts;
+   
+
+myJSON = "";
+
+//giving source URL
+var url = "http://sneakpeeq-sites.s3.amazonaws.com/interviews/ce/feeds/store.js";
+
+//Fetching source data 
+    http.get(url, function(res){
+    var body = '';
+
+        res.on('data', function(chunk){
+            body += chunk;
+        });
+
+        res.on('end', function(){
+            myProducts = JSON.parse(body);
+            myJSON = body;
+            //val = body;
+            foundFlag = false;
+            myProd = "not found";
+            myProdName = "not found";
+            myProdPrice = 0;
+            // comparing the user parameter passed in API call with our product list
+            for(i=0; i < myProducts.products.length; i++) {
+                if(myProducts.products[i].id == req.params.productid) {
+                    myProd = myProducts.products[i].id;
+
+                    switch(req.params.usertype) {
+                        case "retail":
+                            myProdPrice = myProducts.products[i].msrpInCents;
+                            break;
+                        case "wholesale":
+                            myProdPrice = myProducts.products[i].msrpInCents*0.75;
+                            break;
+                        default:
+                            break;
+                    }
+
+                    
+                    myProdName = myProducts.products[i].name;
+                    foundFlag = true;
+                    break;
+                }
+            }
+
+            val = {
+                "products": {
+                    "productid": myProd
+                    , "name": myProdName
+                    , "price": myProdPrice
+                }
+            }; 
+
+            console.log("Got a response: ", myProducts.site);
+     
+        });
+        
+    }).on('error', function(e){
+          console.log("Got an error: ", e);
+    });
+
+    res.status(200).json(val);
+ });
+
 module.exports = router;
